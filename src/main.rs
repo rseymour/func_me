11cb442b3a3b665cfc00bd020801be8c476cea59
Rich Seymour <rich@runsybil.com>
2024-08-19 12:44:43 -0400

    shows the value but the obj is stringy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use serde_json::json;
+use serde_json::Value;
 use stringy_fn_derive_macro::json_signature;
+use stringy_fn_derive_macro::json_value;
 use stringy_fn_derive_macro::print_signature;
 
-#[json_signature]
+//#[json_signature]
+#[json_value]
 fn example(a: i32, b: String) -> Result<(), std::io::Error> {
     dbg!(a);
     dbg!(b);
@@ -11,5 +15,6 @@ fn example(a: i32, b: String) -> Result<(), std::io::Error> {
 
 fn main() {
     example(24, "Hello".to_string()).unwrap();
-    json_call_example();
+    let v = json_value_example();
+    println!("{}", serde_json::to_string_pretty(&v).unwrap());
 }
