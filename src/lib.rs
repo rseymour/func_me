4eb1c68ba1734a76c04b9afe13c5028addc773f5
Rich Seymour <rich@runsybil.com>
2024-08-27 21:06:15 -0400

    ugly but closer to the toolbox idea

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
 
 use derive_quote_to_tokens::ToTokens;
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
 use regex::Regex;
-use syn::{parse_macro_input, FnArg, Ident, ItemFn, Pat, Type};
+use syn::{parse_macro_input, DeriveInput, ItemImpl, LitStr};
+use syn::{FnArg, Ident, ItemFn, Pat, Type};
 
 #[derive(ToTokens)]
 
@@ -65,7 +66,10 @@ fn rust_type_to_json_schema(ty: &Type) -> String {
 }
 
 #[proc_macro_attribute]
-pub fn tool_json_for_fn(_attrs: TokenStream, item: TokenStream) -> TokenStream {
+pub fn tool_json_for_fn(
+    _attrs: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let input = parse_macro_input!(item as ItemFn);
     let attrs = input.attrs.clone();
     let name = &input.sig.ident;
@@ -128,6 +132,142 @@ pub fn tool_json_for_fn(_attrs: TokenStream, item: TokenStream) -> TokenStream {
                     "function": {
                         "name": stringify!(#name),
                         "description": "Description of the function",
+                        "parameters": {
+                            "type": "object",
+                            "required": [#(#required),*],
+                            "properties": {#(#fields),*},
+                        }
+                    }
+                }
+            )
+        }
+    }
+    .into()
+}
+
+//#[proc_macro_derive(IntrospectImpl)]
+//pub fn introspect_impl_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//    let input = parse_macro_input!(input as DeriveInput);
+//    let name = &input.ident;
+//
+//    let expanded = quote! {
+//        impl #name {
+//            pub fn get_impl_names() -> Vec<&'static str> {
+//                vec![]
+//            }
+//        }
+//    };
+//
+//    proc_macro::TokenStream::from(expanded)
+//}
+
+#[proc_macro_attribute]
+pub fn toolbox(
+    _attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(item as ItemImpl);
+    let ty = &input.self_ty;
+
+    let impl_names: Vec<_> = input
+        .items
+        .iter()
+        .filter_map(|item| {
+            if let syn::ImplItem::Fn(method) = item {
+                Some(method.sig.ident.to_string())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let impl_names_tokens = impl_names.iter().map(|name| quote! { #name });
+
+    let expanded = quote! {
+        #input
+
+        impl #ty {
+            pub fn get_impl_names() -> Vec<&'static str> {
+                vec![#(#impl_names_tokens),*]
+            }
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
+}
+
+#[proc_macro_attribute]
+pub fn add_to_toolbox(
+    attribute_args: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    // TODO allow renaming function in the json using an attr?
+    // for now just pull out the function description
+    // TokenStream [Literal { kind: Str, symbol: "this is a secret third function", suffix: None, span: #0 bytes(881..914) }]
+    eprintln!("attrs: {:?}", attribute_args);
+    let function_description = parse_macro_input!(attribute_args as LitStr);
+    let input = parse_macro_input!(item as ItemFn);
+    let attrs = input.attrs.clone();
+    let name = &input.sig.ident;
+    let inputs = &input.sig.inputs;
+    let output = &input.sig.output;
+    let stmts = &input.block.stmts;
+    // this is the function that is inserted into the attributed code
+    // ideally we would add a trait and an impl
+    let json_value = format_ident!("json_value_{}", name);
+    let args = extract_function_raw(&input);
+
+    // these could/should be sets if iteration order is preserved
+    // we rely too much on iteration order
+    let mut fields = Vec::new();
+    let mut required = Vec::new();
+    let mut arg_desc = HashMap::new();
+    let re = Regex::new(r".*?`(?<arg_name>.*?)`\W+(?<arg_description>.*)$").unwrap();
+    //todo create capture groups and us the regex to get the name and description
+    // and push them into a map in raw docs which can be linked/lined up with fields
+    for attr in attrs {
+        match &attr.meta {
+            syn::Meta::NameValue(nv) => {
+                let v = nv.value.clone();
+                match v {
+                    syn::Expr::Lit(s) => match s.lit {
+                        syn::Lit::Str(me) => {
+                            let haystack = me.value();
+                            let arg_caps = re.captures(&haystack).expect("we have doc strings formatted like: /// `arg_name` - arg_description");
+                            arg_desc.insert(
+                                arg_caps["arg_name"].to_string(),
+                                arg_caps["arg_description"].to_string(),
+                            );
+                        }
+                        _ => eprintln!("error in doc string matching code"),
+                    },
+                    _ => (),
+                }
+            }
+            _ => (),
+        }
+    }
+    for arg in args {
+        let name = arg.name.to_string();
+        let arg_type = rust_type_to_json_schema(&arg.arg_type);
+        //let desc = arg.description;
+        let desc = match arg_desc.get(name.as_str()) {
+            Some(desc) => desc,
+            None => "",
+        };
+        let field = quote! {  #name: {"type": #arg_type , "description": #desc} };
+        fields.push(field);
+        required.push(name);
+    }
+    quote! {
+        fn #name(#inputs) #output { #(#stmts)* }
+        fn #json_value() -> Value {
+            json!(
+                {
+                    "type": "function",
+                    "function": {
+                        "name": stringify!(#name),
+                        "description": #function_description,
                         "parameters": {
                             "type": "object",
                             "required": [#(#required),*],
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use serde_json::json;
 use serde_json::Value;
+use stringy_fn_derive_macro::add_to_toolbox;
 use stringy_fn_derive_macro::tool_json_for_fn;
+use stringy_fn_derive_macro::toolbox;
+//use stringy_fn_derive_macro::IntrospectImpl;
 
 #[tool_json_for_fn]
 fn example(a: i32, b: String) -> Result<(), std::io::Error> {
@@ -18,11 +21,35 @@ fn some_other_function(secret_key: String, query: String) -> Result<String, std:
     Ok(format!("{} {}", secret_key, query))
 }
 
+// see https://en.wikipedia.org/wiki/Henry_O._Studley for where the name comes from
+struct StudleyToolChest;
+
+#[toolbox]
+impl StudleyToolChest {
+    pub fn method1() {}
+    pub fn method2() {}
+    pub fn method3() {}
+
+    #[add_to_toolbox("this is a secret third function")]
+    /// * `secret_key` - The secret key used for things
+    /// * `query` - The query you want to ask
+    pub fn some_secret_third_function(
+        secret_key: String,
+        query: String,
+    ) -> Result<String, std::io::Error> {
+        // function body
+        Ok(format!("{} {}", secret_key, query))
+    }
+}
+
 fn main() {
     example(24, "Hello".to_string()).unwrap();
     // the json_value_ prefix is added by the derive macro, but this will become a proper trait/impl
     let v = json_value_example();
     println!("{}", serde_json::to_string_pretty(&v).unwrap());
     let v = json_value_some_other_function();
+    println!("{}", serde_json::to_string_pretty(&v).unwrap());
+    println!("Methods: {:?}", StudleyToolChest::get_impl_names());
+    let v = StudleyToolChest::json_value_some_secret_third_function();
     println!("{}", serde_json::to_string_pretty(&v).unwrap());
 }
