938839d6eca5d9b4e6c34ff8d6a972b422b060bb
Rich Seymour <rseymour@gmail.com>
2024-09-03 12:25:43 -0400

    eprintln debugging getting closer to docs

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,18 @@ fn example(a: i32, b: String) -> Result<(), std::io::Error> {
     Ok(())
 }
 
+/// * `secret_key` - The secret key used for things
+/// * `query` - The query you want to ask
 #[json_value]
 fn some_other_function(secret_key: String, query: String) -> Result<String, std::io::Error> {
+    // function body
+    Ok("Hello".to_string())
+}
+
+/// `secret_key` - The secret key used for things
+/// `query` - The query you want to ask
+#[json_value]
+fn some_other_f2(secret_key: String, query: String) -> Result<String, std::io::Error> {
     // function body
     Ok("Hello".to_string())
 }
